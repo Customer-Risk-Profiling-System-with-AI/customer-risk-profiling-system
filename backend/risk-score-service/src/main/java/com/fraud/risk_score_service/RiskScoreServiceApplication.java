21d3b8168aa297136cbe2b72db3303bd383c8d28
supun-pregeeth <ruchiraniasanka@gmail.com>
2026-02-16 17:03:42 +0530

    Implement Feign client for inter-service communication with Transaction and ML services

--- backend/risk-score-service/src/main/java/com/fraud/risk_score_service/RiskScoreServiceApplication.java
+++ backend/risk-score-service/src/main/java/com/fraud/risk_score_service/RiskScoreServiceApplication.java
@@ -2,8 +2,11 @@ package com.fraud.risk_score_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
 public class RiskScoreServiceApplication {
 
 	public static void main(String[] args) {
